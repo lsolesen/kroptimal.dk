296d775a84a7a5efaa0f051e1ac6a9a56ebff026
Lars Olesen <lars@intraface.dk>
2010-03-08 09:33:55 +0000

    Added kroptimal.

---
+++ src/index.php
@@ -0,0 +1,11 @@
+<?php
+require('/home/retlet/include/retlet_include.php');
+require_once(DOCUMENT_ROOT_CORE. 'Template.php');
+
+$tpl = new Template('./tpl/');
+$tpl->set('title', 'Kroptimal.dk');
+$tpl->set('content_main', 'åbner snart');
+
+echo $tpl->fetch('main-tpl.php');
+
+?>
---
+++ src/tpl/main-tpl.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="da">
+
+<head>
+  <title><?php echo $title; ?></title>
+  <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+  <style type="text/css">@import "/css/style.css";</style>
+</head>
+
+<body>
+<div id="container">
+  <div id="branding">
+    <h1>Kroptimal.dk</h1>
+  </div>
+  
+  <hr />
+  
+  <ul id="navigation-main">
+    <?php echo $navigation; ?>
+  </ul>
+	
+  <hr />
+  
+  <div id="content-main">
+    <?php echo $content_main; ?>
+  </div>
+  
+  <hr />
+  
+  <div id="copyright">
+    &copy; 2005-<?php echo date('Y'); ?> Linda Kruse
+  </div>
+</div>
+</body>
+</html>
